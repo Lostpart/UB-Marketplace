b377bf7ca014b33d5270f41a5a2ff0a63044da44
Shouyue Wu <wushouyue@gmail.com>
2021-10-01 15:24:00 -0400

    Make loginVerification public #32

--- app/src/main/java/com/ubmarketplace/app/manager/UserManager.java
+++ app/src/main/java/com/ubmarketplace/app/manager/UserManager.java
@@ -2,12 +2,15 @@ package com.ubmarketplace.app.manager;
 
 import java.util.logging.Logger;
 
+import com.google.inject.Singleton;
 import lombok.NonNull;
 
+@Singleton
 public class UserManager {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
-    boolean loginVerification(@NonNull String userid, @NonNull String password) {
+    public boolean loginVerification(@NonNull String userId, @NonNull String password) {
+        logger.info(String.format("Verify login information for %s", userId));
         //Call database
 
         return true; //Do nothing at this time
