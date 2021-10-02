0d9c8b3923600e75651aee72593552367fb5e26f
kevinz98 <zhongzj98@gmail.com>
2021-10-01 21:04:53 -0400

    Made server work with POST method form from login.html #30

--- app/src/main/java/com/ubmarketplace/app/controller/LoginCredentialController.java
+++ app/src/main/java/com/ubmarketplace/app/controller/LoginCredentialController.java
@@ -1,39 +1,30 @@
 package com.ubmarketplace.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import com.ubmarketplace.app.manager.*;
+import java.util.*;
 
 
 @Controller
 public class LoginCredentialController {
 
-    public String username, password;
-
-    @RequestMapping("/login")
-    private ModelAndView received(
-            @RequestParam(name = "username") String username,
-            @RequestParam(name = "password") String password
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String recoverPass(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password
     ) {
-        this.username = username;
-        this.password = password;
-
-
-        System.out.println(username);       //only for test
-        System.out.println(password);       //only for test
+        System.out.println(username);   //for testing
+        System.out.println(password);   //for testing
 
         UserManager usermanager = new UserManager();
         boolean valid = usermanager.loginVerification(username, password);
 
-        ModelAndView modelAndView = new ModelAndView();
-
         if (valid) {
-            modelAndView.setViewName("login.html");     //Waiting for the landing screen
+            return "redirect:login.html";     //Waiting for the landing screen
         }
         else {
-            modelAndView.setViewName("login_error.html");
+            return "redirect:login_error.html";
         }
-        return modelAndView;
     }
 
 }
