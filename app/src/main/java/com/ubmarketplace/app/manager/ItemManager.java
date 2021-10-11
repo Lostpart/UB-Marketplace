dfc17cc897a27ad348027db05a9fa1cb2e9298bc
kevinz98 <zhongzj98@gmail.com>
2021-10-10 21:17:14 -0400

    Fixed ItemManager to use AutoWired #37

--- app/src/main/java/com/ubmarketplace/app/manager/ItemManager.java
+++ app/src/main/java/com/ubmarketplace/app/manager/ItemManager.java
@@ -1,16 +1,30 @@
 package com.ubmarketplace.app.manager;
 
+import com.google.inject.Singleton;
 import com.ubmarketplace.app.model.Item;
 import com.ubmarketplace.app.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Singleton
+@Component
 public class ItemManager {
+    final ItemRepository itemRepository;
 
-    ItemRepository ip = new ItemRepository();
+    @Autowired
+    public ItemManager(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+
+
+
+
+//    ItemRepository ip = new ItemRepository();
 
     public List<Item> getALlitem()
     {
-        return ip.findAll();
+        return itemRepository.findAll();
     }
 }
