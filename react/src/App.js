f8a761f5af95203fe4a4041810c29c24524f5ea5
jaykwonproject <jaykwonproject@gmail.com>
2021-10-11 00:45:39 -0400

    Implemented react router domain #39
    Incorporated react router dom functionality to create multiple pages

--- react/src/App.js
+++ react/src/App.js
@@ -1,75 +1,14 @@
-import './App.css';
-import sample from './sample.jpeg'
+  import './App.css';
+  import sample from './sample.jpeg';
+  import Profile from './profile';
+  import Home from './home';
+  import {Route, Link} from "react-router-dom"
 function App() {
 
   return (
       <div className="App">
-
-          <div className="header">
-              <p className="title">UB Marketplace</p>
-              <p className="login">
-                  <a href="http://ubmarketplace.herokuapp.com/login.html">Login/Register</a>
-              </p>
-          </div>
-          <div className="itemList">
-              <div className="itemImg">
-                  <img src={sample}/>
-                  <p>Product Name</p>
-                  <p>Product Price</p>
-              </div>
-              <div className="itemImg">
-                  <img src={sample}/>
-                  <p>Product Name</p>
-                  <p>Product Price</p>
-              </div>
-              <div className="itemImg">
-                  <img src={sample}/>
-                  <p>Product Name</p>
-                  <p>Product Price</p>
-              </div>
-              <div className="itemImg">
-                  <img src={sample}/>
-                  <p>Product Name</p>
-                  <p>Product Price</p>
-              </div>
-              <div className="itemImg">
-                  <img src={sample}/>
-                  <p>Product Name</p>
-                  <p>Product Price</p>
-              </div>
-          </div>
-
-          <div className="categoryList">
-              <h2>Category List</h2>
-              <ul>
-                  <li>Electronics</li>
-                  <li>Textbooks</li>
-                  <li>Clothings</li>
-                  <li>Furnitures</li>
-              </ul>
-              <ul>
-                  <li>Electronics</li>
-                  <li>Textbooks</li>
-                  <li>Clothings</li>
-                  <li>Furnitures</li>
-              </ul>
-              <ul>
-                  <li>Electronics</li>
-                  <li>Textbooks</li>
-                  <li>Clothings</li>
-                  <li>Furnitures</li>
-              </ul>
-              <ul>
-                  <li>Electronics</li>
-                  <li>Textbooks</li>
-                  <li>Clothings</li>
-                  <li>Furnitures</li>
-              </ul>
-              <ul>
-                  <li>See All Items</li>
-                  <li>Sell My Items</li>
-              </ul>
-          </div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/profile" component={Profile} />
       </div>
   );
 }
