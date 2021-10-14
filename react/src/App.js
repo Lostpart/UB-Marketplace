4ce696613b26742e6904221172707c59bb2571c9
beetosu <cpwalsh@buffalo.edu>
2021-10-14 13:32:57 -0400

    Attempt using hashrouter #34

--- react/src/App.js
+++ react/src/App.js
@@ -3,19 +3,19 @@
   import Profile from './profile';
   import Home from './home';
   import Login from './login';
-  import {Route, Link, BrowserRouter, Switch} from "react-router-dom"
+  import {Route, Link, BrowserRouter, Switch, HashRouter} from "react-router-dom"
 import Register from './register';
 function App() {
 
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/profile" component={Profile} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
