3867d8b73bfd4c5b17ab2626e13eeab9e0b49799
beetosu <cpwalsh@buffalo.edu>
2021-10-14 13:54:17 -0400

    Remove unneeded imports #34

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,8 @@
   import './App.css';
-  import sample from './sample.jpeg';
   import Profile from './profile';
   import Home from './home';
   import Login from './login';
-  import {Route, Link, BrowserRouter, Switch, HashRouter} from "react-router-dom"
+  import {Route, Switch, HashRouter} from "react-router-dom"
 import Register from './register';
 function App() {
 
