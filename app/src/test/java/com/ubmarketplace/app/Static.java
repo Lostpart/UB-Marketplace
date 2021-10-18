4a9b869add6c1c9dfa3acf7ec2e78b38925c3bac
ChangHoSung <changhos@buffalo.edu>
2021-10-18 01:37:10 -0400

    Update static for testing addNewUser and addNewItem functions #43

--- app/src/test/java/com/ubmarketplace/app/Static.java
+++ app/src/test/java/com/ubmarketplace/app/Static.java
@@ -2,17 +2,23 @@ package com.ubmarketplace.app;
 
 public class Static {
     final static public String TEST_USER_NAME_1 = "UnitTest_TestUser1@test.com";
-    final static public String TEST_PASSWORD_1 = "0a446a38175032f569a8c63dfb7ca4a1a82a7a072cb22b8e5ae58d77447cec44"; //Before hash: GoodPassword
-    final static public String TEST_USER_DISPLAY_NAME_1 = "DisplayName1";
+    final static public String TEST_PASSWORD_1 = "GoodPassword";
     final static public String TEST_USER_NAME_2 = "UnitTest_TestUser2@test.com";
-    final static public String TEST_PASSWORD_2 = "02a8cfdc38de40f29ca5b3a680b2eb1affd7bd370f86807219a0574b576fd7e4"; //Before hash: GoodPassword2
-    final static public String TEST_USER_DISPLAY_NAME_2 = "DisplayName2";
+    final static public String TEST_PASSWORD_2 = "GoodPassword2";
+    final static public String TEST_USER_NAME_3 = "UnitTest_TestUser3@test.com";
+    final static public String TEST_PASSWORD_3 = "GoodPassword3";
     final static public String TEST_ALWAYS_WRONG_PASSWORD = "BadPassword";
 
     final static public String TEST_ITEM_ID_1 = "55188c206c4e44e6977f75631a55c047";
     final static public String TEST_ITEM_NAME_1 = "UnitTest_TestItem1";
     final static public String TEST_ITEM_ID_2 = "68c8cd3624f94fd684c3ca7899731403";
     final static public String TEST_ITEM_NAME_2 = "UnitTest_TestItem2";
+    final static public String TEST_ITEM_ID_3 = "7b686b06e46b41c6998a1c120b966365";
+    final static public String TEST_ITEM_NAME_3 = "UnitTest_TestItem3";
+    final static public String TEST_ITEM_DESCRIPTION_3 = "Laptop";
+    final static public Double TEST_ITEM_PRICE_3 = 150.0;
+    final static public String TEST_ITEM_IMAGE_3 = "https://cdn.pixabay.com/photo/2014/05/02/21/49/laptop-336373_1280.jpg";
+    final static public String TEST_ITEM_MEETINGPLACE_3 = "Student Union";
 
     final static public String TEST_MONGO_URL = "mongodb://%s:%d";
     final static public String TEST_MONGO_DATABASE_NAME = "UnitTest_Database";
