7bcf07b22f05568268b358643f6880b3a50476bd
ChangHoSung <changhos@buffalo.edu>
2021-10-21 23:23:54 -0400

    Update NewItemController #40

--- app/src/main/java/com/ubmarketplace/app/controller/NewItemController.java
+++ app/src/main/java/com/ubmarketplace/app/controller/NewItemController.java
@@ -1,60 +1,58 @@
 package com.ubmarketplace.app.controller;
 
+import com.ubmarketplace.app.dto.NewItemRequest;
+import com.ubmarketplace.app.dto.NewItemResponse;
 import com.ubmarketplace.app.manager.ItemManager;
-import com.ubmarketplace.app.manager.UserManager;
 import com.ubmarketplace.app.model.Item;
-import com.ubmarketplace.app.model.User;
-import com.ubmarketplace.app.repository.ItemRepository;
-import com.ubmarketplace.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class NewItemController {
 
-    final UserManager userManager;
     final ItemManager itemManager;
-    final UserRepository userRepository;
-    final ItemRepository itemRepository;
 
     @Autowired
-    public NewItemController(UserManager userManager, ItemManager itemManager, UserRepository userRepository, ItemRepository itemRepository){
-        this.userManager = userManager;
+    public NewItemController(ItemManager itemManager){
         this.itemManager = itemManager;
-        this.userRepository = userRepository;
-        this.itemRepository = itemRepository;
     }
 
-    @RequestMapping(value = "/api/newitem", method = RequestMethod.POST)
-
-    //waiting for FE stuff of listing new Item
-    /*private NewItemResponse newItem(@RequestBody NewItemRequest newItemRequest){
+    @RequestMapping(value = "/api/newItem", method = RequestMethod.POST)
+     private NewItemResponse newItem(@RequestBody NewItemRequest newItemRequest){
         Item item = itemManager.addNewItem(
                 newItemRequest.getName(),
                 newItemRequest.getOwner(),
                 newItemRequest.getDescription(),
-                newItemReqeust.getPrice(),
+                newItemRequest.getPrice(),
                 newItemRequest.getImageFilePath(),
-                newItemRequest.getMeetingPlace())
+                newItemRequest.getMeetingPlace());
+
+        return NewItemResponse.builder().item(item).build();
+    }
+
+    //For local test
+    /*private NewItemResponse newItem(){
+        List<String> images = new ArrayList<String>();
+        images.add("https://book1");
+        images.add("https://book2");
+        User owner = User.builder().username("kyle").build();
+
+        newItemRequest => it assumes that it receives new item data information from FE
+        NewItemRequest newItemRequest = new NewItemRequest("book", owner, "This is book", 10.0, images, "SU");
+
+
+        Item item = itemManager.addNewItem(
+                newItemRequest.getName(),
+                newItemRequest.getOwner(),
+                newItemRequest.getDescription(),
+                newItemRequest.getPrice(),
+                newItemRequest.getImageFilePath(),
+                newItemRequest.getMeetingPlace());
 
         return NewItemResponse.builder().item(item).build();
     }*/
-
-    private String received(){
-        User user1 = User.builder().username("kyle").password("asdasd").build();
-        userRepository.insert(user1);
-
-        Item item1 = new Item();
-        item1.setDescription("This is a Laptop");
-        item1.setName("Laptop");
-        item1.setMeetingPlace("Student Union");
-        item1.setOwner(user1);
-        item1.setImageFilePath("https://cdn.pixabay.com/photo/2014/05/02/21/49/laptop-336373_1280.jpg");
-        item1.setPrice(150.0);
-        itemRepository.insert(item1);
-
-        return "redirect:/allitem";
-    }
 }
