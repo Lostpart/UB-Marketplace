b6e5b73886096b35b1df15d72836ef248c52d506
jaykwonproject <jaykwonproject@gmail.com>
2021-10-31 19:03:16 -0400

    Removed dummy json, Replaced with real api call #54

--- react/src/home.js
+++ react/src/home.js
@@ -4,6 +4,7 @@ import "./Carousel.css";
 import './home.css';
 import Carousel from "./Carousel"
 import Header from "./header";
+import {handleAPIError} from "./errors";
 
 
 class home extends Component{
@@ -40,21 +41,36 @@ class home extends Component{
 
 
 
-        fetch('http://ubmarketplace-develop.herokuapp.com/api/allitem',requestOptions)
-            .then(res=>{
-                this.setState({
-                    isLoaded: true,
-                    //items: res.json
-                    items: [{"itemId":"1","name":"Item Name1","owner":{"username":"abc@buffalo.edu","password":"123456"},"description":"This is a book","price":25.0,"imageFilePath":"https://images.unsplash.com/photo-1626885228113-0ac4b52e6cea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"North","createdTime":12345},
-                        {"itemId":"2","name":"Item Name2","owner":{"username":"zzhong","displayName":"123456"},"description":"This is a book","price":11.0,"imageFilePath":"https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80","meetingPlace":"North","createdTime":12345},
-                        {"itemId":"3","name":"Item Name3","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":12.0,"imageFilePath":"https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"4","name":"Item Name4","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":41.0,"imageFilePath":"https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"5","name":"Item Name5","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":231.0,"imageFilePath":"https://images.unsplash.com/photo-1566813916511-2701d4c96576?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3570&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"6","name":"Item Name6","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":54.0,"imageFilePath":"https://images.unsplash.com/photo-1526738549149-8e07eca6c147?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1925&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"7","name":"Item Name7","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":123.0,"imageFilePath":"https://images.unsplash.com/photo-1627719172031-ab42dc849bc3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"8","name":"Item Name8","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":22.0,"imageFilePath":"https://images.unsplash.com/photo-1518611507436-f9221403cca2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1925&q=80","meetingPlace":"South campus","createdTime":114514},
-                        {"itemId":"9","name":"Item Name9","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":5.0,"imageFilePath":"https://images.unsplash.com/photo-1531347520814-e80b3cbe3cba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1066&q=80","meetingPlace":"South campus","createdTime":114514}]
-                })
+        // fetch('/api/allitem',requestOptions)
+        //     .then(res=>{
+        //         this.setState({
+        //             isLoaded: true,
+        //             //items: res.json
+        //             items: [{"itemId":"1","name":"Item Name1","owner":{"username":"abc@buffalo.edu","password":"123456"},"description":"This is a book","price":25.0,"imageFilePath":"https://images.unsplash.com/photo-1626885228113-0ac4b52e6cea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"North","createdTime":12345},
+        //                 {"itemId":"2","name":"Item Name2","owner":{"username":"zzhong","displayName":"123456"},"description":"This is a book","price":11.0,"imageFilePath":"https://images.unsplash.com/photo-1593642532973-d31b6557fa68?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1480&q=80","meetingPlace":"North","createdTime":12345},
+        //                 {"itemId":"3","name":"Item Name3","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":12.0,"imageFilePath":"https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"4","name":"Item Name4","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":41.0,"imageFilePath":"https://images.unsplash.com/photo-1593642632559-0c6d3fc62b89?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2069&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"5","name":"Item Name5","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":231.0,"imageFilePath":"https://images.unsplash.com/photo-1566813916511-2701d4c96576?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3570&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"6","name":"Item Name6","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":54.0,"imageFilePath":"https://images.unsplash.com/photo-1526738549149-8e07eca6c147?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1925&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"7","name":"Item Name7","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":123.0,"imageFilePath":"https://images.unsplash.com/photo-1627719172031-ab42dc849bc3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"8","name":"Item Name8","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":22.0,"imageFilePath":"https://images.unsplash.com/photo-1518611507436-f9221403cca2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1925&q=80","meetingPlace":"South campus","createdTime":114514},
+        //                 {"itemId":"9","name":"Item Name9","owner":{"username":"abc@buffalo.edu","displayName":"12345678"},"description":"This is a car","price":5.0,"imageFilePath":"https://images.unsplash.com/photo-1531347520814-e80b3cbe3cba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1066&q=80","meetingPlace":"South campus","createdTime":114514}]
+        //         })
+        //     })
+
+        fetch("https://ubmarketplace-develop.herokuapp.com/api/allitem",requestOptions)
+            .then(response=> {
+                if (response.status !== 200) {
+                    handleAPIError(response);
+
+                } else {
+                    response.json().then(data => {
+                        console.log(data);
+                        this.setState({
+                            isLoaded: true,
+                            items: data.item})
+                    });
+                }
             })
     }
 
@@ -64,15 +80,14 @@ class home extends Component{
         if(name==null){
             return (
                 <div className="home">
-                    <Header />
-
+                    <Header/>
                     <div className="itemList">
                         <Carousel  show={5} infiniteLoop>
                             {items.map(item=>(
                                 <div className="itemImg">
                                     {/*link needs to be uuid ex) item.uuid */}
                                     <Link to="/item">
-                                    <img src={item.imageFilePath}/></Link>
+                                    <img src={item.images}/></Link>
                                     <p>{item.name}</p>
                                     <p>${item.price}</p>
 
@@ -108,7 +123,7 @@ class home extends Component{
                             <li>Furnitures</li>
                         </ul>
                         <ul>
-                            <li>See All Items</li>
+                            <li><Link to="/listing">See All Items</Link></li>
                             <li><Link to="/sell">Sell My Items</Link></li>
                         </ul>
                     </div>
@@ -130,7 +145,6 @@ class home extends Component{
                             </div>
                         </div>
                     </div>
-
                     <div className="itemList">
                         <Carousel  show={5} infiniteLoop>
                             {items.map(item=>(
@@ -172,8 +186,8 @@ class home extends Component{
                             <li>Furnitures</li>
                         </ul>
                         <ul>
-                            <li>See All Items</li>
-                            <li>Sell My Items</li>
+                            <li><Link to="/listing">See All Items</Link></li>
+                            <li><Link to="/sell">Sell My Items</Link></li>
                         </ul>
                     </div>
                 </div>
