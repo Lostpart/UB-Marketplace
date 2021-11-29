fee70ffc308852f28cbce78a7a30c42e9fbf6f54
beetosu <cpwalsh@buffalo.edu>
2021-11-29 15:01:05 -0500

    Store user role on login #78

--- react/src/login.js
+++ react/src/login.js
@@ -34,8 +34,12 @@ class Login extends React.Component {
                 if (response.status !== 200) {
                     handleAPIError(response)
                 } else {
-                    localStorage.setItem("email", this.state.userId);
-                    this.props.history.push({pathname:"/",state: {name: this.state.username}})
+                    response.json()
+                        .then(data => {
+                            localStorage.setItem("email", data.userId);
+                            localStorage.setItem("role", data.role);
+                            this.props.history.push({ pathname:"/",state: {name: this.state.username} })
+                        })
                 }
             });
         
