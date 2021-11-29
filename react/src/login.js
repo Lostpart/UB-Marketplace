c7568b4dcebbf0519d4e6379a2b9ee506034f881
jaykwonproject <jaykwonproject@gmail.com>
2021-11-28 20:39:37 -0500

    Added displayname in localStorage #75

--- react/src/login.js
+++ react/src/login.js
@@ -34,6 +34,9 @@ class Login extends React.Component {
                 if (response.status !== 200) {
                     handleAPIError(response)
                 } else {
+                    response.json().then(data => {
+                        localStorage.setItem("username", data.user.displayName);
+                    });
                     localStorage.setItem("email", this.state.userId);
                     this.props.history.push({pathname:"/",state: {name: this.state.username}})
                 }
